8c33bbdc1c5cc7c6c6c4f4fc3da210ebb755705c
pjmendoz <pearl.j.mendoza@gmail.com>
2019-09-26 14:53:08 -0400

    adding in clear

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const canvas = document.getElementById("gameScreen");
 
 const ctx = canvas.getContext("2d");
 
+ctx.clearRect(0, 0, 1400, 725)
+
 ctx.fillStyle = '#DAF7A6 '
-ctx.fillRect(20, 20, 20, 20); 
+ctx.fillRect(60, 20, 20, 20); 
 console.log('hi')
 

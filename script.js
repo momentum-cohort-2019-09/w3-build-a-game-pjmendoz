e6f4779fb7b2a902407d46e7159d04e6d420472c
pjmendoz <pearl.j.mendoza@gmail.com>
2019-09-27 13:47:42 -0400

    adding coin

--- script.js
+++ script.js
@@ -3,32 +3,28 @@ class Game {
         const canvas = document.getElementById(gameScreen)
         this.square = canvas.getContext('2d')
         this.player= canvas.getContext('2d')
+        this.coin = canvas.getContext('2d')
     }
     update() {
         this.fillBackground()
         this.person()
+        this.point()
     }    
     fillBackground() {
         this.square.fillStyle= '#DAF7A6';
         this.square.fillRect(200, 180, 200, 200);
     }
-    person (){
+    person(){
         this.player.clearRect (0, 0, 200, 200)
         this.player.fillStyle= '#581845'
         this.player.fillRect(210, 190, 50, 50)
+    }
+    point() {
+        this.player.clearRect (0, 0, 200, 200)
+        this.coin.fillStyle = '#FFC300'
+        this.coin.fillRect(210, 250, 10, 10)
     }
 }
 
 let game = new Game("gameScreen")
 game.update()
-
-// const canvas = document.getElementById("gameScreen");
-// const ctx = canvas.getContext("2d");
-
-// ctx.fillStyle = '#DAF7A6'
-// ctx.fillRect(200, 180, 200, 200); 
-
-// ctx.clearRect (0, 0, 200, 200)
-// ctx.fillStyle = '#581845'
-// ctx.fillRect(210, 190, 50, 50)
-      
